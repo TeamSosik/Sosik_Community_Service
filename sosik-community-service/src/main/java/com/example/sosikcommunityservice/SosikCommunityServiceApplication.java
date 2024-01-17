6f0eecef58ffae4467552dc5799d2a47a61c6f43
ComNewbie <iforking3@naver.com>
2024-01-17 12:35:03 +0900

    #feat 게시판 서비스 초기 설정
    게시판 서비스 초기설정하였습니다!

---
+++ sosik-community-service/src/main/java/com/example/sosikcommunityservice/SosikCommunityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.sosikcommunityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosikCommunityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosikCommunityServiceApplication.class, args);
+	}
+
+}
