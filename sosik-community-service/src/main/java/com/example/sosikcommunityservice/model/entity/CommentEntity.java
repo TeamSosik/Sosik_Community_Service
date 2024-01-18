0c3b12d3ac1baabe8dd1e83cb58aea211ce409b5
hyeonji <hyunji11092@gmail.com>
2024-01-18 16:44:30 +0900

    feat : 커뮤니티 게시판 entity 추가
    커뮤니티 게시판 entity 추가하였습니다.

---
+++ sosik-community-service/src/main/java/com/example/sosikcommunityservice/model/entity/CommentEntity.java
@@ -0,0 +1,35 @@
+package com.example.sosikcommunityservice.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comment")
+public class CommentEntity extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "community_id", nullable = false)
+    private CommunityEntity community;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(length = 1000, nullable = false)
+    private String content;
+
+    @Builder
+    public CommentEntity(
+            final Long memberId,
+            final String content
+    ) {
+        this.memberId = memberId;
+        this.content = content;
+
+    }
+}
