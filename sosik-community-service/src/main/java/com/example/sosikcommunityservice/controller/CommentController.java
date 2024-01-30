f95f9f924138d7448d841b36182276b3ff1b9d56
hyeonji <hyunji11092@gmail.com>
2024-01-30 17:17:15 +0900

    refactor : 댓글 작성 주소 변경
    댓글 작성 주소를 변경하였습니다.

--- sosik-community-service/src/main/java/com/example/sosikcommunityservice/controller/CommentController.java
+++ sosik-community-service/src/main/java/com/example/sosikcommunityservice/controller/CommentController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
     private final CommentService commentService;
 
-    @PostMapping("/create")
+    @PostMapping
     public Result<ResponseCreateComment> createComment(@RequestHeader Long memberId, @RequestBody RequestCreateComment comment) {
         ResponseCreateComment responseCreateComment = commentService.createComment(memberId,comment);
         return Result.success(responseCreateComment);
